f679e1630e7bb5a19f832b9cb59a4a0b086b6dee
Ryan Flitcroft <ryanflitcroft@ymail.com>
2021-12-06 16:40:46 -0800

    cleaned up comments in app.js, test written and passing for render-utils functions

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// import functions and grab DOM elements
-import { renderGame, renderTeam } from './render-utils.js';
+import { renderGame } from './render-utils.js';
 const currentGameEl = document.getElementById('current-game-container');
 const pastGamesEl = document.getElementById('past-games-container');
 
@@ -12,8 +11,6 @@ const finishGameButton = document.getElementById('finish-game-button');
 const teamOneLabel = document.getElementById('team-one-name');
 const teamTwoLabel = document.getElementById('team-two-name');
 
-// create an array to hold on to the state of past games
-
 let name1 = '';
 let name2 = '';
 let score1 = 0;
@@ -21,43 +18,35 @@ let score2 = 0;
 let pastGamesArray = [];
 
 nameForm.addEventListener('submit', (e) => {
-    // don't forget to prevent the default form behavior!
     e.preventDefault();
-    // get the name data from the form
+
     const data = new FormData(nameForm);
-    // set the state to this data from the form
+
     name1 = data.get('team-one');
     name2 = data.get('team-two');
-    // reset the form values
+
     nameForm.reset();
     displayCurrentGameEl();
-// console.log(name1, name2);
 });
 
 teamOneAddButton.addEventListener('click', () => {
-    // increment the current state for team one's score
     score1++;
     displayCurrentGameEl();
-    
 });
 
 teamTwoAddButton.addEventListener('click', () => {
-    // increment the current state for team two's score
     score2++;
     displayCurrentGameEl();
 });
 
 teamOneSubtractButton.addEventListener('click', () => {
-    // decrement the current state for team one's score
     score1--;
     displayCurrentGameEl();
 });
 
 teamTwoSubtractButton.addEventListener('click', () => {
-    // decrement the current state for team two's score
     score2--;
     displayCurrentGameEl();
-    
 });
 
 finishGameButton.addEventListener('click', () => {
@@ -68,20 +57,15 @@ finishGameButton.addEventListener('click', () => {
         score2
     };
     pastGamesArray.push(game);
-    // console.log(pastGamesArray);
-    // add the current game to an array of games in state.
-    // HINT: it will be helpful to keep track of these games as objects with 4 properties, one for each piece of state we're tracking
 
     displayAllGames();
 
-    // reset the initial state to start with a new form
     name1 = '';
     name2 = '';
     score1 = 0;
     score2 = 0;
     displayCurrentGameEl();
 });
-
 
 function displayCurrentGameEl() {
     const game = {
@@ -91,30 +75,22 @@ function displayCurrentGameEl() {
         score2
     };
 
-    // clear out the current game div
     currentGameEl.textContent = '';
-    // change the label to show team one's name;
     teamOneLabel.textContent = name1;
-    // change the label to show team two's name;
     teamTwoLabel.textContent = name2;
-    // console.log(currentGameEl);
-    // call the render game function to create a game element
+
     let gameEl = renderGame(game);
-    // console.log(gameEl);
-    // append the element to the cleared out current game div
     currentGameEl.append(gameEl);
 }
 
 
 function displayAllGames() {
-    // clear out the past games list in the DOM
     pastGamesEl.textContent = '';
-    // loop through the past games in state
+
     for (let past of pastGamesArray) {
         let pastGame = renderGame(past);
         pastGamesEl.append(pastGame);
     }
-    // render and append a past game for each past game in state
 }
 
 
--- render-utils.js
+++ render-utils.js
@@ -1,35 +1,13 @@
-// consider: what data type is game here? where will you need to pass it this data type in your app?
 export function renderGame(game) {
-    //creates an element div, stored in variable 'div'
-    //calls renderTeam(game.name1, game.score1), stored in variable team1Div
-    //calls renderTeam(game.name2, game.score2), stored in variable team2Div
-    //appends team1Div, team2Div to div
-    //returns div, containing team1Div team2Div 
     const div = document.createElement('div');
-    //renderTeam(name, score);
     const team1Div = renderTeam(game.name1, game.score1);
-        //returns <div> <p>name</p> <p>score</p> </div>
     const team2Div = renderTeam(game.name2, game.score2);
-        //returns <div> <p>name</p> <p>score</p> </div>
 
     div.append(team1Div, team2Div);
-
     div.classList.add('game');
 
     return div;
-    // renderGame(game) 
-        // returns <div> <p>name1</p> <p>score1</p> </div> 
-        //         <div> <p>name2</p> <p>score2</p> </div>
 }
-
-//creates element div, stored in variable teamDiv
-//creates element p, stored in variable nameDiv
-//creates element p, stored in variable scoreDiv
-//nameDiv text content is equal to the argument passed in name
-//scoreDiv text content is equal to the argument passed in scoreDiv
-//nameDiv and scoreDiv are appended to teamDiv div 
-//returns teamDiv
-// ** teamDiv = <div> <p>name</p> <p>score</p> </div> **
 
 export function renderTeam(name, score) {
     const teamDiv = document.createElement('div');
